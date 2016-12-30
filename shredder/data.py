44eb6bf89617feca603067d170e5c7579132aff9
wzpfish <wzpfish@gmail.com>
2016-12-30 12:20:52 +0800

    first commit

--- shredder/data.py
+++ shredder/data.py
@@ -1,80 +1,83 @@
 #coding: utf-8
 
 class Dataset(object):
-	def __init__(self, filename):
-		self.num_features = 0
-		self.samples = self._load_data(filename)
-		self.sample_indices = list(range(0, len(self.samples[0])))
-		self.feature_indices = list(range(0, self.num_features))
+  """Class to restore the data and provide some convenient
+  functions to access the targets and features.
+  """
+  def __init__(self, filename):
+    self.num_features = 0
+    self.samples = self._load_data(filename)
+    self.sample_indices = list(range(0, len(self.samples[0])))
+    self.feature_indices = list(range(0, self.num_features))
 
-	def _load_data(self, filename):
-		samples = [[], []] #labels, features
-		with open(filename) as fin:
-			for line in fin:
-				line = line.rstrip('\n')
-				if not line: continue
-				fields = line.split('\t')
-				label = float(fields[0])
-				features = [float(it) for it in fields[1:]]
-				if self.num_features == 0:
-					self.num_features = len(features)
-				elif self.num_features != len(features):
-					raise ValueError('Every sample must have same dimension of features. Expected dim %s, got dim %s'
-						% (self.num_features, len(features)))
-				samples[0].append(label)
-				samples[1].append(features)
-		return samples
+  def _load_data(self, filename):
+    samples = [[], []] #labels, features
+    with open(filename) as fin:
+      for line in fin:
+        line = line.rstrip('\n')
+        if not line: continue
+        fields = line.split('\t')
+        label = float(fields[0])
+        features = [float(it) for it in fields[1:]]
+        if self.num_features == 0:
+          self.num_features = len(features)
+        elif self.num_features != len(features):
+          raise ValueError('Every sample must have same dimension of features. Expected dim %s, got dim %s'
+            % (self.num_features, len(features)))
+        samples[0].append(label)
+        samples[1].append(features)
+    return samples
 
-	def get_targets(self):
-		return self.samples[0]
+  def get_targets(self):
+    return self.samples[0]
 
-	def get_target_at(self, index):
-		return self.samples[0][index]
-		
-	def get_sample_indices(self):
-		return self.sample_indices
+  def get_target_at(self, index):
+    return self.samples[0][index]
+    
+  def get_sample_indices(self):
+    return self.sample_indices
 
-	def get_features(self):
-		return self.samples[1]
+  def get_features(self):
+    return self.samples[1]
 
-	def get_feature_indices(self):
-		return self.feature_indices
+  def get_feature_indices(self):
+    return self.feature_indices
 
-	def get_feature_values_of_samples(self, sample_indices, feature_idx):
-		feature_values = []
-		for sample_idx in sample_indices:
-			value = self.samples[1][sample_idx][feature_idx]
-			feature_values.append(value)
-		return feature_values
+  def get_feature_values_of_samples(self, sample_indices, feature_idx):
+    feature_values = []
+    for sample_idx in sample_indices:
+      value = self.samples[1][sample_idx][feature_idx]
+      feature_values.append(value)
+    return feature_values
 
 
 # Test codes.
 def gen_dummy_file():
-	content = ('1\t0.1\t0.2\t0.3\t0.4\n'
-			   '0\t0.5\t0.6\t0.7\t0.8\n'
-			   '0\t0.9\t1.0\t1.1\t1.2\n')
-	dummy_filename = './dummy.txt'
-	with open(dummy_filename, 'w') as fout:
-		fout.write(content)
-	return dummy_filename
+  content = ('1\t0.1\t0.2\t0.3\t0.4\n'
+         '0\t0.5\t0.6\t0.7\t0.8\n'
+         '0\t0.9\t1.0\t1.1\t1.2\n')
+  dummy_filename = './dummy.txt'
+  with open(dummy_filename, 'w') as fout:
+    fout.write(content)
+  return dummy_filename
 
 
 def test_test():
-	dummy_filename = gen_dummy_file()
-	dataset = Dataset(dummy_filename)
-	import os
-	os.remove(dummy_filename)
-	
-	sample_indices = dataset.get_sample_indices()
-	assert sample_indices == [0, 1, 2]
-	feature_indices = dataset.get_feature_indices()
-	assert feature_indices == [0, 1, 2, 3]
-	feature_values = dataset.get_feature_values_of_samples([0, 1, 2], 0)
-	assert feature_values == [0.1, 0.5, 0.9]
-	feature_values = dataset.get_feature_values_of_samples([0, 1, 2], 3)
-	assert feature_values == [0.4, 0.8, 1.2]
-	print('Test passed.')
+  dummy_filename = gen_dummy_file()
+  dataset = Dataset(dummy_filename)
+  import os
+  os.remove(dummy_filename)
+  
+  sample_indices = dataset.get_sample_indices()
+  assert sample_indices == [0, 1, 2]
+  feature_indices = dataset.get_feature_indices()
+  assert feature_indices == [0, 1, 2, 3]
+  feature_values = dataset.get_feature_values_of_samples([0, 1, 2], 0)
+  assert feature_values == [0.1, 0.5, 0.9]
+  feature_values = dataset.get_feature_values_of_samples([0, 1, 2], 3)
+  assert feature_values == [0.4, 0.8, 1.2]
+  print('Test passed.')
 
 
 if __name__ == '__main__':
-	test_test()
+  test_test()
--- shredder/gbm.py
+++ shredder/gbm.py
@@ -6,74 +6,82 @@ from . import tree
 from . import loss
 
 class GBDT(object):
-	def __init__(self, num_trees, learning_rate, max_num_of_leaf_nodes, loss_type, criterion_type='mse'):
-		self.num_trees = num_trees
-		self.learning_rate = learning_rate
-		self.max_num_of_leaf_nodes = max_num_of_leaf_nodes
-		self.loss = loss.loss_of_type(loss_type)
-		self.criterion_type = criterion_type
+  """The GBDT model.
+  """
+  def __init__(self, num_trees, learning_rate, max_num_of_leaf_nodes, loss_type, criterion_type='mse'):
+    self.num_trees = num_trees
+    self.learning_rate = learning_rate
+    self.max_num_of_leaf_nodes = max_num_of_leaf_nodes
+    self.loss = loss.loss_of_type(loss_type)
+    self.criterion_type = criterion_type
 
-		self.predict_value = []
-		self.trees = []
+    self.predict_value = []
+    self.trees = []
 
-	
-	def init_train(self, dataset):
-		# Init the F0 value of every samples.
-		self.loss.init_predict_value(self.predict_value, dataset)
+  def init_train(self, dataset):
+    # Init the F0 value of every samples.
+    self.loss.init_predict_value(self.predict_value, dataset)
 
-	def train_iter(self, dataset):
-		pseudo_targets = self._calc_residual(dataset)
-		base_tree = tree.Tree(self.max_num_of_leaf_nodes, self.loss, self.criterion_type)
-		base_tree.build(dataset, pseudo_targets)
-		self.trees.append(base_tree)
-		self.update_predict_value(base_tree)
-		train_loss = self._compute_loss(dataset)
-		return train_loss
+  def train_iter(self, dataset):
+    pseudo_targets = self._calc_residual(dataset)
+    base_tree = tree.Tree(self.max_num_of_leaf_nodes, self.loss, self.criterion_type)
+    base_tree.build(dataset, pseudo_targets)
+    self.trees.append(base_tree)
+    self.update_predict_value(base_tree)
+    train_loss = self._compute_loss(dataset)
+    return train_loss
 
-	def update_predict_value(self, tree):
-		leaf_nodes = tree.get_leaf_nodes()
-		for node in leaf_nodes:
-			sample_indices = node.get_samples()
-			for sample_idx in sample_indices:
-				self.predict_value[sample_idx] += self.learning_rate * node.get_predict_value()
+  def update_predict_value(self, tree):
+    """Update the current Fm(xi) for each i in training samples.
+    """
+    leaf_nodes = tree.get_leaf_nodes()
+    for node in leaf_nodes:
+      sample_indices = node.get_samples()
+      for sample_idx in sample_indices:
+        self.predict_value[sample_idx] += self.learning_rate * node.get_predict_value()
 
-	def feature_importance(self, dataset):
-		importances = [0. for it in dataset.get_feature_indices()]
-		for tree in self.trees:
-			for node in tree.non_leaf_nodes:
-				importances[node.split_feature_id] += node.split_improvement
-		sum_import = sum(importances)
-		norm = [it / sum_import for it in importances]
-		return norm
+  def feature_importance(self, dataset):
+    importances = [0. for it in dataset.get_feature_indices()]
+    for tree in self.trees:
+      for node in tree.non_leaf_nodes:
+        importances[node.split_feature_id] += node.split_improvement
+    sum_import = sum(importances)
+    norm = [it / sum_import for it in importances]
+    return norm
 
-	def _calc_residual(self, dataset):
-		residuals = []
-		sample_indices = dataset.get_sample_indices()
-		for sample_idx in sample_indices:
-			target = dataset.get_target_at(sample_idx)
-			residual = 2. * target / (1 + math.exp(2 * target * self.predict_value[sample_idx]))
-			residuals.append(residual)
-		return residuals
+  def _calc_residual(self, dataset):
+    """Calculate the pseudo target i.e. residual
+    """
+    residuals = []
+    sample_indices = dataset.get_sample_indices()
+    for sample_idx in sample_indices:
+      target = dataset.get_target_at(sample_idx)
+      residual = 2. * target / (1 + math.exp(2 * target * self.predict_value[sample_idx]))
+      residuals.append(residual)
+    return residuals
 
-	def _compute_loss(self, dataset):
-		targets = dataset.get_targets()
-		predicts = self.predict_value
-		return self.loss.compute_loss(targets, predicts)
+  def _compute_loss(self, dataset):
+    targets = dataset.get_targets()
+    predicts = self.predict_value
+    return self.loss.compute_loss(targets, predicts)
 
-	def accuracy(self, dataset, threshold=0.5):
-		targets = dataset.get_targets()
-		features = dataset.get_features()
-		mean = numpy.mean(targets)
-		true_count = 0
-		for idx, feature in enumerate(features):
-			predict_value = mean
-			for tree in self.trees:
-				predict_value += tree.predict(feature)
-			target = targets[idx]
-			prob = 1.0 / (1 + math.exp(-2 * predict_value))
-			predict_label = -1
-			if prob >= threshold:
-				predict_label = 1
-			if predict_label == target:
-				true_count += 1
-		return true_count * 1.0 / len(targets)
+  def accuracy(self, dataset, threshold=0.5):
+    """For evaluate.
+    May be implement out of this file.
+    """
+    targets = dataset.get_targets()
+    features = dataset.get_features()
+    mean = numpy.mean(targets)
+    true_count = 0
+    for idx, feature in enumerate(features):
+      predict_value = mean
+      for tree in self.trees:
+        predict_value += tree.predict(feature)
+      target = targets[idx]
+      prob = 1.0 / (1 + math.exp(-2 * predict_value))
+      predict_label = -1
+      if prob >= threshold:
+        predict_label = 1
+      if predict_label == target:
+        true_count += 1
+    return true_count * 1.0 / len(targets)
